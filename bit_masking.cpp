2d3c46ff4d4dc610eed7f6873fd7568e012fd24b
Negin2468 <Nkarimi0397@conestogac.on.ca>
2024-11-27 23:06:06 -0500

    Create bit_masking.cpp
    making only the function prototype then our code know about them

---
+++ bit_masking.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+unsigned char setBit(unsigned char data, int bit);
+unsigned char clearBit(unsigned char data, int bit);
+unsigned char toggleBit(unsigned char data, int bit);
+
+int main()
+{
+return 0;
+}
